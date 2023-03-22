a4918d155b7380a386311e2b82f49cefe9aa749a
Hai Nghia <nghiahh.nta@gmail.com>
2023-03-22 16:45:25 +0700

    Route
    Signed-off-by: Hai Nghia <nghiahh.nta@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # save this as app.py
-from flask import Flask, request
+from flask import Flask, request, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -7,35 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = request.args.get("name", "<script>alert(\"bad\")</script>")
-    return f'Hellzzzo, {escape(name)}!'
+    return 'index'
 
 
-@app.route('/hello')
-def hello():
-    return '<h1>hello</h1>'
+@app.route('/login')
+def login():
+    return 'login'
 
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {escape(username)}'
+def profile(username):
+    return f'{username} \'s profile'
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'Post {post_id}'
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    return f'Subpath {escape(subpath)}'
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/abount')
-def abount():
-    return 'The abount page'
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    # print(url_for('login', next='/'))
+    print(url_for('profile', username='Hai Nghia'))
