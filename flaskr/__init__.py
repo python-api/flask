d50dcfaad8087b4906a0bb22a475d279935bdd4d
Hai Nghia <nghiahh.nta@gmail.com>
2023-03-23 11:11:41 +0700

    Deploy
    Signed-off-by: Hai Nghia <nghiahh.nta@gmail.com>

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='b8a314c69fc565813cf6b6b0583da72251e691e21591ae3077e409899a64d7b0',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
     )
     if test_config is None:
