d8083b3738be9a30307beb304f87ceb1d128d26d
Hai Nghia <hohainghia19@gmail.com>
2023-03-28 15:45:46 +0700

    Config
    Signed-off-by: Hai Nghia <hohainghia19@gmail.com>

--- src/app/config/database.py
+++ src/app/config/database.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,6 +7,12 @@ db = SQLAlchemy()
 
 
 def connect_to_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+    DB_USERNAME = os.environ.get('DB_USERNAME')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = os.environ.get('DB_HOST')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{username}:password@localhost/db_name'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/db_name"
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
