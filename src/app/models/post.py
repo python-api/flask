148a690c7a8f44b69ccfd16c2c284a8687412237
Hai Nghia <hohainghia19@gmail.com>
2023-03-31 14:59:35 +0700

    migrate

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -3,9 +3,8 @@ from src import database
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    title = database.Column(database.String(250))
+    title = database.Column(database.String(200))
     content = database.Column(database.Text)
-    contents = database.Column(database.Text)
 
     def __repr__(self):
         return f'<Post "{self.title}">'
