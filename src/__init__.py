f533c1f8d16f9c758af636d9df84c211292d6eac
Hai Nghia <hohainghia19@gmail.com>
2023-06-24 15:10:29 +0700

    m

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,19 @@ from flask_migrate import Migrate
 from src.config import Config
 from src.extensions import Extensions, database
 from src.routes import version
+
+
 extension = Extensions()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Initialize Flask extensions here
+    database.init_app(app)
+    migrate = Migrate(app, database)
+    from src.database import models
+
 
     # Initialize Flask extensions here
     extension.database.init_app(app=app)
--- src/extensions/__init__.py
+++ src/extensions/__init__.py
@@ -1,6 +1,12 @@
-from flask_marshmallow import Marshmallow
-from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint
+from src.extensions.sqlalchemy import database
+extensions = Blueprint('extensions', __name__)
 
-class Extensions(object):
-    marshmallow = Marshmallow()
-    database = SQLAlchemy()
+
+class Extensions:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.before_request(...)
