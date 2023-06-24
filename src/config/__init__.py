73452d3c973edd69fbcbc62c4dcbe12838516819
Hai Nghia <hohainghia19@gmail.com>
2023-06-24 16:04:59 +0700

    update

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,6 +2,14 @@ import os
 
 from flask import Blueprint
 from sqlalchemy import create_engine
+from sqlalchemy import orm as sa_orm
+from sqlalchemy_redshift.dialect import TIMESTAMPTZ, TIMETZ
+
+
+
+import sqlalchemy as sa
+from sqlalchemy.engine.url import URL
+
 
 config = Blueprint('config', __name__)
 
@@ -15,11 +23,27 @@ class Config(object):
     DB_DATABASE = os.environ.get('DB_DATABASE')
     DB_USERNAME = os.environ.get('DB_USERNAME')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = f'{DB_CONNECTION}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
+    # SQLALCHEMY_DATABASE_URI = f'{DB_CONNECTION}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATE_FOLDER = 'src/templates'  # đường dẫn đến thư mục templates
 
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    # build the sqlalchemy URL
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        drivername=DB_CONNECTION,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE,
+        username=DB_USERNAME,
+        password=DB_PASSWORD
+    )
+
+    engine = sa.create_engine(SQLALCHEMY_DATABASE_URI)
+
+    Session = sa_orm.sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    # engine = create_engine(SQLALCHEMY_DATABASE_URI)
     try:
         with engine.connect() as connection:
             print("Kết nối Redshift thành công!")
